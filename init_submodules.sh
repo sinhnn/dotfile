c5b94522fa82800d1ac210c1e3e73fbf9507b85e
sinhnn <sinh.e2357@gmail.com>
2017-07-05 13:52:48 +0700

    Init vim config

---
+++ init_submodules.sh
@@ -0,0 +1,26 @@
+# -----------------------------------------------------------------------------
+# Project name   :
+# File name      : init_submodules.sh
+# Created date   : Wed 05 Jul 2017 01:52:02 PM +07
+# Author         : Ngoc-Sinh Nguyen
+# Last modified  : Wed 05 Jul 2017 01:52:02 PM +07
+# Guide          :
+# -----------------------------------------------------------------------------
+#!/bin/bash
+git submodule add https://github.com/Raimondi/delimitMate.git .vim/bundle/delimitMate
+git submodule add https://github.com/nachumk/systemverilog.vim .vim/bundle/systemverilog.vim
+git submodule add https://github.com/vim-syntastic/syntastic .vim/bundle/syntastic
+git submodule add https://github.com/tpope/vim-surround.git .vim/bundle/vim-surround
+git submodule add https://github.com/scrooloose/nerdtree.git .vim/bundle/nerdtree
+git submodule add https://github.com/godlygeek/tabular.git .vim/bundle/tabular
+git submodule add https://github.com/vim-airline/vim-airline.git .vim/bundle/vim-airline
+git submodule add https://github.com/vim-scripts/Conque-GDB .vim/bundle/Conque-GDB
+git submodule add https://github.com/vim-scripts/DoxygenToolkit.vim .vim/bundle/DoxygenToolkit.vim
+git submodule add https://github.com/airblade/vim-gitgutter .vim/bundle/vim-gitgutter
+git submodule add https://github.com/rhysd/vim-clang-format .vim/bundle/vim-clang-format
+git submodule add https://github.com/SirVer/ultisnips .vim/bundle/ultisnips
+git submodule add https://github.com/ervandew/supertab .vim/bundle/supertab
+git submodule add https://github.com/tmhedberg/matchit .vim/bundle/matchit
+git submodule add https://github.com/majutsushi/tagbar .vim/bundle/tagbar
+git submodule add https://github.com/vim-scripts/OmniCppComplete .vim/bundle/OmniCppComplete
+git submodule add https://github.com/Yggdroot/indentLine .vim/bundle/indentLine
