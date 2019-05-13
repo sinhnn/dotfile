006f6f3973a2ca3734cce8f30b0bd59e497dc57e
sinhnn <sinhnn.92@gmail.com>
2019-05-13 09:06:51 +0700

    env python2, python3

---
+++ window-10/env/python2.sh
@@ -0,0 +1,3 @@
+export PYTHONHOME="/c/Python27/"
+export PYTHONPATH="/c/Python27/Lib"
+export PATH="$PYTHONHOME:$PYTHONHOME/Scripts/:$PATH"
---
+++ window-10/env/python3.sh
@@ -0,0 +1,3 @@
+export PYTHONHOME="/c/Python3/"
+export PYTHONPATH="/c/Python3/Lib"
+export PATH="$PYTHONHOME:$PYTHONHOME/Scripts/:$PATH"
